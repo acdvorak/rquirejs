cd5371a309405806848b233d9f10ffd9e11b1b09
Andrew C. Dvorak <acdvorak@gmail.com>
2014-05-01 14:57:32 -0500

    Removed absolute paths from test config

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 var Compiler = require('./src/compiler/compiler.js');
 
 var compiler = new Compiler({
-    "src_root": "/Users/acdvorak/dev/libs/rquirejs/example/src",
+    "src_root": "example/src/",
     "main": "main.js",
     "modules_dir": "modules/",
     "base_modules": [
 //        "array",
 //        "counter"
     ],
-    "dest": "/Users/acdvorak/dev/libs/rquirejs/example/dist/example.js"
+    "dest": "example/dist/example.js"
 });
 
 compiler.compile();
